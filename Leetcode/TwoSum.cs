acaf05839f1ffe45eb0f318b87b69e529fd10025
Shreya Verma <vermashreya165@gmail.com>
2021-10-26 23:03:21 -0700

    Create TwoSum.cs

---
+++ Leetcode/TwoSum.cs
@@ -0,0 +1,71 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
+
+//Example 1:
+
+//Input: nums = [2, 7, 11, 15], target = 9
+//Output:[0,1]
+//Output: Because nums[0] +nums[1] == 9, we return [0, 1].
+
+//Example 2:
+
+//Input: nums = [3, 2, 4], target = 6
+//Output:[1,2]
+
+//Example 3:
+
+//Input: nums = [3, 3], target = 6
+//Output:[0,1]
+
+
+using System;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    public class TwoSum
+    {
+        public int[] TwoSumSolution_1(int[] nums, int target)
+        {
+            int[] result = null;
+            if (nums == null || nums.Length < 2)
+                return result;
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var curr = nums[i];
+                var complement = target - nums[i];
+                if (dict.ContainsKey(complement))
+                {
+                    result = new int[] { dict[complement], i };
+                    break;
+                }
+                if (!dict.ContainsKey(curr))
+                    dict[curr] = i;
+            }
+
+            return result;
+        }
+
+        public int[] TwoSumSolution_2(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { dict[target - nums[i]], i };
+                }
+                else
+                {
+                    dict[nums[i]] = i;
+                }
+            }
+            return null;
+        }
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
